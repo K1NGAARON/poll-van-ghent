9923086de5997d783ba7f692155e46c5f5114391
K1NGAARON <gerardaaron1997@gmail.com>
2022-10-15 13:56:46 +0200

    gallery

---
+++ js/script.js
@@ -0,0 +1,27 @@
+$("#menu-toggle").click(function() {
+    $(".small-menu-wrapper").toggle("active");
+});
+
+let cookieBanner = document.querySelector(".cookie-banner-wrapper");
+let cookiesAccepted = document.querySelector("#accept-cookies");
+let blackOverlay = document.querySelector("#overlay");
+
+// Accept cookies
+cookiesAccepted.addEventListener("click", function acceptCookies() {
+    blackOverlay.classList.add("hide-cookie-banner");
+    cookieBanner.classList.add("hide-cookie-banner");
+    storeCookieConsent();
+});
+
+function storeCookieConsent() {
+    localStorage.cookies = "accepted";
+}
+
+window.addEventListener("DOMContentLoaded", function() {
+    if ("cookies" in localStorage) {
+        blackOverlay.classList.add("hide-cookie-banner");
+        cookieBanner.classList.add("hide-cookie-banner");
+    } else {
+        // Shows cookieBanner
+    }
+});
