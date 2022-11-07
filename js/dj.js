64e6ec44c9c9dbf2d948b734a81da9a395e59a52
K1NGAARON <gerardaaron1997@gmail.com>
2022-11-07 20:11:20 +0100

    script

--- js/dj.js
+++ js/dj.js
@@ -48,33 +48,26 @@ const artists = [
         'name' : 'Koen',
         'style' : 'Stijl',
         'about' : 'Koen is een xyz',
-    },
-]
+    }
+];
 
 // Create client logos section
 function createDJ() {
     for (let i = 0; i < artists.length; i++) {
         const artistTemplate = `
-        
-        <div class="item">
-            <img class="artist" src="${artists[i].img}" alt="${artists[i].name}">
-            <h3 class="name">
-                ${artists[i].name}
-            </h3>
-            <p class="style">
-                ${artists[i].style}
-            </p>
-            <p class="about">
-                ${artists[i].about}
-            </p>
-        </div>
-
-
+            <div class="item">
+                <img class="artist" src="${artists[i].img}" alt="${artists[i].name}">
+                <h3 class="name">
+                    ${artists[i].name}
+                </h3>
+                <p class="style">
+                    ${artists[i].style}
+                </p>
+                <p class="about">
+                    ${artists[i].about}
+                </p>
+            </div>
         `; 
-        artistHolder.insertAdjacentHTML("beforeend", artistTemplate);
+        artistHolder.insertAdjacentHTML("afterbegin", artistTemplate);
     };
 };
-
-$(document).ready(function() {
-    createDJ();
-});
