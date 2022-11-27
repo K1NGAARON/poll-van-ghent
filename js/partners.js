60f1205fae702da54b27221efa6a270e9a26074c
K1NGAARON <gerardaaron1997@gmail.com>
2022-11-27 11:23:11 +0100

    script

--- js/partners.js
+++ js/partners.js
@@ -62,22 +62,100 @@ const partners = [
         'url' : 'https://www.chelsy.be/'
     },
     {
-        'name' : '',
-        'category' : '',
-        'img' : '../img/partners/',
-        'url' : ''
+        'name' : 'Alex Vdb',
+        'category' : 'fotograaf',
+        'img' : '../img/partners/alex-vdb.jpg',
+        'url' : 'https://www.alexvdb.be/'
     },
     {
-        'name' : '',
-        'category' : '',
-        'img' : '../img/partners/',
-        'url' : ''
+        'name' : 'Ipon',
+        'category' : 'fotograaf',
+        'img' : '../img/partners/ipon.jpg',
+        'url' : 'https://ipon.be/'
     },
     {
-        'name' : '',
-        'category' : '',
-        'img' : '../img/partners/',
-        'url' : ''
+        'name' : 'Annelies',
+        'category' : 'fotograaf',
+        'img' : '../img/partners/annelies.jpg',
+        'url' : 'https://annelies.photos/'
+    },
+    {
+        'name' : 'Foto Pruvoost',
+        'category' : 'fotograaf',
+        'img' : '../img/partners/pruvoost.jpg',
+        'url' : 'https://www.fotopruvoost.be/'
+    },
+    {
+        'name' : 'Foto Aan Huis',
+        'category' : 'fotograaf',
+        'img' : '../img/partners/foto-aan-huis.jpg',
+        'url' : 'https://fotoaanhuis.be/'
+    },
+    {
+        'name' : 'Eye Catcher Photography',
+        'category' : 'fotograaf',
+        'img' : '../img/partners/eyecatcher.jpg',
+        'url' : 'https://www.eyecatcherphotography.be/'
+    },
+    {
+        'name' : 'Dm Fotografie',
+        'category' : 'fotograaf',
+        'img' : '../img/partners/dm.jpg',
+        'url' : 'https://www.dmfotografie.be/'
+    },
+    {
+        'name' : 'Andy Hauman',
+        'category' : 'fotograaf',
+        'img' : '../img/partners/andy.jpg',
+        'url' : 'https://www.fotografie-hauman.be/'
+    },
+    {
+        'name' : 'David Deman',
+        'category' : 'fotograaf',
+        'img' : '../img/partners/david.jpg',
+        'url' : 'https://www.daviddeman.be/'
+    },
+    {
+        'name' : 'Kevin Dandenberghe',
+        'category' : 'fotograaf',
+        'img' : '../img/partners/kevin.jpg',
+        'url' : 'https://www.kevin-v.com/'
+    },
+    {
+        'name' : 'Ik Film Je',
+        'category' : 'video',
+        'img' : '../img/partners/ikfilmje.jpg',
+        'url' : 'https://www.ikfilmje.be/'
+    },
+    {
+        'name' : 'SV Video',
+        'category' : 'video',
+        'img' : '../img/partners/sv.jpg',
+        'url' : 'http://www.sv-video.be/'
+    },
+    {
+        'name' : 'As Video',
+        'category' : 'video',
+        'img' : '../img/partners/as.jpg',
+        'url' : 'http://www.asvideo.be/'
+    },
+    {
+        'name' : 'Duchateau Films',
+        'category' : 'video',
+        'img' : '../img/partners/duchateau.jpg',
+        'url' : 'https://www.duchateaufilms.be/'
+    },
+    {
+        'name' : 'RVD Videografie',
+        'category' : 'video',
+        'img' : '../img/partners/rvd.jpg',
+        'url' : 'http://rvdvideografie.be/'
+    },
+    {
+        'name' : 'Van Wonterghem Catering',
+        'category' : 'catering',
+        'img' : '../img/partners/wonterghem.jpg',
+        'url' : 'https://vanwonterghemcatering.be/'
     },
     {
         'name' : '',
