8a57f7c0a7544f7bb5acce535e85fdb6e8bbe2e1
K1NGAARON <gerardaaron1997@gmail.com>
2022-11-27 10:52:49 +0100

    script

--- js/partners.js
+++ js/partners.js
@@ -22,8 +22,80 @@ const partners = [
     {
         'name' : 'Claeys & Duerinck',
         'category' : 'ceremonie',
-        'img' : '../img/partners/',
+        'img' : '../img/partners/clayes.jpg',
         'url' : 'https://www.ceremonie-claeysduerinck.be/'
+    },
+    {
+        'name' : 'Danny David',
+        'category' : 'ceremonie',
+        'img' : '../img/partners/danny-david.jpg',
+        'url' : 'https://dannydavid.be/ceremoniemeester/'
+    },
+    {
+        'name' : 'Ceremonie Mondy',
+        'category' : 'ceremonie',
+        'img' : '../img/partners/mondy.jpg',
+        'url' : 'http://www.ceremonie-mondy.be/'
+    },
+    {
+        'name' : 'Ceremoniekledij Vanwynsberghe',
+        'category' : 'kledij',
+        'img' : '../img/partners/kleding.jpg',
+        'url' : 'https://www.ceremoniekledijvanwynsberghe.be/'
+    },
+    {
+        'name' : 'Sfeermomenten',
+        'category' : 'zang',
+        'img' : '../img/partners/sfeermomenten.png',
+        'url' : 'https://www.sfeermomenten.be/'
+    },
+    {
+        'name' : 'Abo Songs',
+        'category' : 'zang',
+        'img' : '../img/partners/abo-songs.jpg',
+        'url' : 'https://www.abosongs.be/'
+    },
+    {
+        'name' : 'Chelsy',
+        'category' : 'zang',
+        'img' : '../img/partners/chelsy.jpg',
+        'url' : 'https://www.chelsy.be/'
+    },
+    {
+        'name' : '',
+        'category' : '',
+        'img' : '../img/partners/',
+        'url' : ''
+    },
+    {
+        'name' : '',
+        'category' : '',
+        'img' : '../img/partners/',
+        'url' : ''
+    },
+    {
+        'name' : '',
+        'category' : '',
+        'img' : '../img/partners/',
+        'url' : ''
+    },
+    {
+        'name' : '',
+        'category' : '',
+        'img' : '../img/partners/',
+        'url' : ''
+    },
+    {
+        'name' : '',
+        'category' : '',
+        'img' : '../img/partners/',
+        'url' : ''
+    },
+    {
+        'name' : '',
+        'category' : '',
+        'img' : '../img/partners/',
+        'url' : ''
     },
     {
         'name' : '',
